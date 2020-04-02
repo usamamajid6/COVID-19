12d9f521ae437f35fe545a029f5fa873cbe92291
Usama Majid <usamamajid6@gmail.com>
2020-04-02 08:40:55 +0500

    Table Added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,28 +3,45 @@ import axios from "axios";
 import "./Home.css";
 import NumberFormat from "react-number-format";
 import NumberToWord from "number-to-words";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 class Home extends React.Component {
   state = {
     totalCases: 0,
     totalDeaths: 0,
-    totalRecovered: 0
+    totalRecovered: 0,
+    rows: []
   };
   componentDidMount = async () => {
-    const result = await axios({
+    let result = await axios({
       method: "get",
       url: `https://corona.lmao.ninja/all`,
       headers: {
         "content-type": "application/json"
       }
     });
-    
     this.setState({
-        totalCases:result.data.cases,
-        totalDeaths:result.data.deaths,
-        totalRecovered:result.data.recovered
-    })
-    // this.sortResult(result.data.data);
+      totalCases: result.data.cases,
+      totalDeaths: result.data.deaths,
+      totalRecovered: result.data.recovered
+    });
+    result = await axios({
+      method: "get",
+      url: `https://corona.lmao.ninja/countries`,
+      headers: {
+        "content-type": "application/json"
+      }
+    });
+    this.setState({
+      rows: result.data
+    });
+    console.log(this.state.rows);
   };
 
   sortResult = result => {
@@ -50,11 +67,13 @@ class Home extends React.Component {
       totalRecovered
     });
   };
+
   render() {
+    // const classes = this.useStyles();
     return (
       <div>
         <div className="mainHead">CORONA VIRUS(COVID-19) STATUS</div>
-        <div className="mainFigures" style={{ color: "cyan" }}>
+        <div className="mainFigures" style={{ color: "blue" }}>
           <div className="mainFiguresFigure">
             <NumberFormat
               value={this.state.totalCases}
@@ -84,7 +103,7 @@ class Home extends React.Component {
             ({NumberToWord.toWords(this.state.totalDeaths)})
           </div>
         </div>
-        <div className="mainFigures" style={{ color: "lightgreen" }}>
+        <div className="mainFigures" style={{ color: "green" }}>
           <div className="mainFiguresFigure">
             <NumberFormat
               value={this.state.totalRecovered}
@@ -99,6 +118,46 @@ class Home extends React.Component {
             ({NumberToWord.toWords(this.state.totalRecovered)})
           </div>
         </div>
+        <TableContainer className="tableStyle" component={Paper}>
+          <Table size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Country Name</TableCell>
+                <TableCell align="center">Cases</TableCell>
+                <TableCell align="center">Today Cases</TableCell>
+                <TableCell align="center">Deaths</TableCell>
+                <TableCell align="center">Today Deaths</TableCell>
+                <TableCell align="center">Recovered</TableCell>
+                <TableCell align="center">Active</TableCell>
+                <TableCell align="center">Critical</TableCell>
+                <TableCell align="center">CP1M</TableCell>
+                <TableCell align="center">DP1M</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.rows.map((row, i) => (
+                <TableRow key={i}>
+                  <TableCell align="center" component="th" scope="row">
+                    {row.country}
+                  </TableCell>
+                  <TableCell align="center">{row.cases}</TableCell>
+                  <TableCell align="center">{row.todayCases}</TableCell>
+                  <TableCell align="center">{row.deaths}</TableCell>
+                  <TableCell align="center">{row.todayDeaths}</TableCell>
+                  <TableCell align="center">{row.recovered}</TableCell>
+                  <TableCell align="center">{row.active}</TableCell>
+                  <TableCell align="center">{row.critical}</TableCell>
+                  <TableCell align="center">
+                    {row.casesPerOneMillion}
+                  </TableCell>
+                  <TableCell align="center">
+                    {row.deathsPerOneMillion}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     );
   }
